23642c1d03fdcf1e9823daaee60a41ffdf756fe1
arakawayosiki <yamato4tennis@gmail.com>
2021-07-19 14:05:18 +0000

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_094327) do
+ActiveRecord::Schema.define(version: 2021_07_19_140333) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
