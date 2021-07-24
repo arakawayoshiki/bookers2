2d6acb2ce6d5fbfda009269629d8149386a4a14b
arakawayosiki <yamato4tennis@gmail.com>
2021-07-24 12:59:15 +0000

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @books = @user.post_images.page(params[:page]).reverse_order
+    @books = @user.books.page(params[:page]).reverse_order
   end
 
   def show
